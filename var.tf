fe340f049afa494b54e1cb3094ed39b8af48b86f
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-01 12:26:41 +0530

    sadf

--- var.tf
+++ var.tf
@@ -8,37 +8,41 @@ variable "component" {
       name = "mongodb"
       type = "t3.micro"
     }
-    catalogue = {
-      name = "catalogue"
-      type = "t3.micro"
-    }
     redis = {
       name = "redis"
+      type = "t3.micro"
+    }
+     mysql = {
+      name = "mysql"
       type = "t3.micro"
     }
     user = {
       name = "user"
       type = "t3.micro"
     }
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
     cart = {
       name = "cart"
       type = "t3.micro"
     }
-    mysql = {
-      name     = "mysql"
-      type     = "t3.micro"
-    }
     shipping = {
-      name     = "shipping"
-      type     = "t3.micro"
+      name = "shipping"
+      type = "t3.micro"
     }
     rabbitmq = {
-      name     = "rabbitmq"
-      type     = "t3.micro"
+      name = "rabbitmq"
+      type = "t3.micro"
     }
     payment = {
-      name     = "payment"
-      type     = "t3.micro"
+      name = "payment"
+      type = "t3.micro"
+    }
+    dispatch = {
+      name = "dispatch"
+      type = "t3.micro"
     }
   }
 }
