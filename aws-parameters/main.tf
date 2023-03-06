3ae025d86fa5879702d5e1841bfae3ff97bd8895
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-06 12:37:23 +0530

    sdf

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,7 +1,8 @@
 resource "aws_ssm_parameter" "test1" {
-  name  = var.parameters.name
-  type  = var.parameters[0].type
-  value = var.parameters[0].value
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
 }
 
 variable "parameters" {}
