fd370a36e5ace8127d2718ac6381c8a99a6f998a
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 21:05:19 +0530

    dfg

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ dev-plan: git
 	terraform plan -var-file=env-dev/dev.tfvars
 
 dev-apply: git
+	cd aws-parameters; make dev-apply
 	terraform init -backend-config=env-dev/state.tfvars
 	terraform apply -var-file=env-dev/dev.tfvars -auto-approve
 
--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
     monitor = try( each.value["monitor"], false )
 } */
 
-module "myvpcm" {
+/* module "myvpcm" {
     source = "git::https://github.com/shankarsrinivasnew/tf-module-vpc.git"
     for_each =  var.vpc
     vpc_cidr = each.value["vpc_cidr"]
@@ -17,8 +17,22 @@ module "myvpcm" {
     private_subnets = each.value["private_subnets"]
     default_vpc_id = var.default_vpc_id
     default_route_table = var.default_route_table
-}
+} */
 
-output "mylocalo" {
+/* output "mylocalo" {
   value = local.db_subnet_ids
+} */
+
+module "docdbm" {
+  source     = "git::https://github.com/shankarsrinivasnew/tf-module-docdb.git"
+  env        = var.env
+  tags       = var.tags
+  subnet_ids = local.db_subnet_ids
+
+  for_each                = var.docdb
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
 }
