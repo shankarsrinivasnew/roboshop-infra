4872ebd194cefed92f3c3a6d103ce3487a0a0696
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 22:48:54 +0530

    dsfg

--- locals.tf
+++ locals.tf
@@ -1,4 +1,6 @@
 locals {
   db_subnet_ids = tolist([module.myvpcm["main"].myoutsub_private["db-az1"].id,module.myvpcm["main"].myoutsub_private["db-az2"].id])
+  app_subnet_ids = tolist([module.myvpcm["main"].myoutsub_private["app-az1"].id,module.myvpcm["main"].myoutsub_private["app-az2"].id])
+  web_subnet_ids = tolist([module.myvpcm["main"].myoutsub_private["web-az1"].id,module.myvpcm["main"].myoutsub_private["web-az2"].id])
 }
 
