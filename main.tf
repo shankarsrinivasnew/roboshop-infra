8b1cc14cb19fb9da6ac71dfd781c454e7798b29f
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 21:16:41 +0530

    dsfg

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
     monitor = try( each.value["monitor"], false )
 } */
 
-/* module "myvpcm" {
+module "myvpcm" {
     source = "git::https://github.com/shankarsrinivasnew/tf-module-vpc.git"
     for_each =  var.vpc
     vpc_cidr = each.value["vpc_cidr"]
@@ -17,7 +17,7 @@
     private_subnets = each.value["private_subnets"]
     default_vpc_id = var.default_vpc_id
     default_route_table = var.default_route_table
-} */
+}
 
 /* output "mylocalo" {
   value = local.db_subnet_ids
