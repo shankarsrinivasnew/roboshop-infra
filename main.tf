98ebd8601877931adf44e8fc55711c7024ce2ac5
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-22 21:40:45 +0530

    dfg

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@
 } */
 
 module "myvpcm" {
-    source = "git::https://github.com/shankarsrinivasnew/tf-module-vpc.git"
-    for_each =  var.vpc
-    vpc_cidr = each.value["vpc_cidr"]
-    env = var.env
-    tags = var.tags
-    public_subnets = each.value["public_subnets"]
-    private_subnets = each.value["private_subnets"]
-    default_vpc_id = var.default_vpc_id
-    default_route_table = var.default_route_table
+  source              = "git::https://github.com/shankarsrinivasnew/tf-module-vpc.git"
+  for_each            = var.vpc
+  vpc_cidr            = each.value["vpc_cidr"]
+  env                 = var.env
+  tags                = var.tags
+  public_subnets      = each.value["public_subnets"]
+  private_subnets     = each.value["private_subnets"]
+  default_vpc_id      = var.default_vpc_id
+  default_route_table = var.default_route_table
 }
 
 /* output "mylocalo" {
@@ -35,4 +35,7 @@ module "docdbm" {
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
+  storage_encrypted       = each.value["storage_encrypted"]
+  instance_count          = each.value["instance_count"]
+  instance_class          = each.value["instance_class"]
 }
