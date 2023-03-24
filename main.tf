88a62065896f3b27ad41515ccd3dd34aa7ff127b
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-24 14:21:43 +0530

    sadf

--- main.tf
+++ main.tf
@@ -122,5 +122,6 @@ module "asgm" {
   max_size         = each.value["max_size"]
   min_size         = each.value["min_size"]
   subnets          = lookup(local.asg_subnet_ids, each.value["subnet_name"], null)
+  port             = each.value["port"]
   allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
 }
